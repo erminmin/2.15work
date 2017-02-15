9a3ed1b15bc2de8ea5d259c42bead4b5c1336c76
min <min>
2017-02-15 23:48:16 +0800

    02.15 work

---
+++ 1.php
@@ -0,0 +1,17 @@
+<?php
+// // echo($_GET["name"]$_GET["age"]);
+// if($_POST["name"]=="zhangsan"){
+// 	echo "ok";
+// }else{
+// 	echo "error";
+// }
+// $str="我是:";
+// $str1="jiamin";
+// echo ($str.$str1);
+$aa=$_GET["aa"];
+$bb=$_GET["bb"];
+
+
+echo $aa.$bb;
+
+?>
